86c0722982f1de58f9602463911d840918bcecaa
Mauricio Costa <mauricio.costa@sensedata.com.br>
2022-02-20 23:44:30 -0300

    html inicial

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Post
 
 
+# template body: https://blog01.superservidor.info/
+# https://blog.wedologos.com.br/exemplos-de-blogs/
 def main(request):
     pub = Post.objects.order_by('-publiched_date')
     context = {'posts': pub}
